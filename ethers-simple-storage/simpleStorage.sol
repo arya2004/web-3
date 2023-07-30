36b26486a616eaaf477573746dcc9115672c4f7d
arya2004 <arya20j4@gmail.com>
2023-07-30 12:58:09 +0530

    basic contract

--- ethers-simple-storage/simpleStorage.sol
+++ ethers-simple-storage/simpleStorage.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.8;
+pragma solidity 0.8.7;
 
 // pragma solidity ^0.8.0;
 // pragma solidity >=0.8.0 <0.9.0;
@@ -23,8 +23,8 @@ contract SimpleStorage {
 
     function retrieve() public view returns (uint256) {
         return favoriteNumber;
+    
     }
-
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
