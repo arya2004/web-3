9f85e46567660a1e65b70ac852e28988088e6396
arya2004 <arya20j4@gmail.com>
2023-10-23 22:54:17 +0530

    17:48:00

--- next_smart/components/LotteryEntrance.jsx
+++ next_smart/components/LotteryEntrance.jsx
@@ -1,20 +1,45 @@
 //add func to enter lottery
 
 import { useWeb3Contract } from "react-moralis"
-import {abi, contractAddress} from "../Constants"
+import {abi, contractAddresses} from "../Constants"
 import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
 
 
 export default function LotteryEntrance(){
-    const {chainID} = useMoralis()
+    const {chainId: chainIdHex, isWeb3Enabled} = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+
+    const {}
     // const {runContractFunction: enterRaffle} = useWeb3Contract({
     //     abi: abi,
-    //     contractAddress: contractAddress, //specify netword id
-    //     functionName: "observe",
+    //     contractAddress: raffleAddress, //specify netword id
+    //     functionName: "enterRaffle",
     //     params: {},
-    //     msgValue:
+    //     msgValue: "d"
 
     // })
+    const {runContractFunction: getEntranceFee} = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, //specify netword id
+        functionName: "getEntranceFee",
+        params: {}
+
+    })
+
+
+
+    useEffect(() => {
+        if(isWeb3Enabled){
+            //try read entrance fee
+            async function updateUI(){
+                const something = await getEntranceFee()
+                console.log(something);
+            }
+            updateUI();
+        }
+    },[isWeb3Enabled])
 
     return(
         <div>LE</div>
